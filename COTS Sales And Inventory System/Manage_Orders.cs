194f92806baab8e99a2030e98fb194e1d45a5dc8
jpfrost <jpfrost15@gmail.com>
2015-09-07 18:12:11 +0800

    back to working

--- COTS Sales And Inventory System/Manage_Orders.cs
+++ COTS Sales And Inventory System/Manage_Orders.cs
@@ -49,82 +49,11 @@ namespace COTS_Sales_And_Inventory_System
         private void RefreshFilters()
         {
             
-            }
+        }
 
         private void RefreshGrid()
         {
-            var datatable =
-                DatabaseConnection.GetCustomTable(
-                    "SELECT orders.idorderlist as 'OrderList ID'" +
-                    ",OrderID as 'Order ID'" +
-                    ",items.Item_Name as 'Product Name'" +
-                    ",size.Size as 'product size'" +
-                    ",OrderQty as 'Quantity'" +
-                    ",date_format(date.Date, '%d/%m/%Y') as 'Date'" +
-                    "FROM cotsalesinventory.orders " +
-                    "inner join Size on orders.SizeID=Size.SizeID " +
-                    "inner join items on items.ItemID=Size.ItemID " +
-                    "inner join date on date.DateID=orders.DateID " +
-                    "inner join orderlist on orders.idorderList=orderlist.idorderList " +
-                    "where orderDelivered=false;","tblOrder");
-            dataGridView1.DataSource = datatable;
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            ReceiveOrder();
-        }
-
-        private void ReceiveOrder()
-        {
-            var receive = new ReceiveOrder();
-            if (receive.ShowDialog(this) == DialogResult.OK)
-            {
-                var orderListID = receive.cueTextBox1.Text;
-                InputOrders(orderListID);
-            }
-           
-        }
-
-        private void InputOrders(string orderListId)
-        {
-            var orders =
-                DatabaseConnection.GetCustomTable(
-                    "SELECT items.ItemID," +
-                    "size.sizeID," +
-                    "orders.OrderQty," +
-                    "orders.idorderlist"+
-                    " FROM items " +
-                    "inner join Size on items.ItemID=size.ItemID " +
-                    "inner join Orders on Size.SizeID=Orders.SizeID " +
-                    "inner join orderlist on Orders.idorderList=orderlist.idorderList; ",
-                    "receiveOrders");
-            var receiveOrders = orders.Select("idorderlist ='"+ orderListId+"'");
-            InsertOrders(receiveOrders);
-
-        }
-
-        private void InsertOrders(DataRow[] receiveOrders)
-        {
-            foreach (DataRow orderRow in receiveOrders)
-            {
-                var itemID = orderRow["itemID"].ToString();
-                var sizeID = Convert.ToInt32(orderRow["sizeID"]);
-                var quantity = Convert.ToInt32(orderRow["orderQty"]);
-                var Size = DatabaseConnection.DatabaseRecord.Tables["Size"].Select("itemID ='"+itemID
-                    +"' and sizeId ='"+sizeID+"'");
-                int initialQty = GetQuantity(Size[0]["quantity"]);
-                initialQty += quantity;
-                Size[0]["quantity"] = initialQty;
-                DatabaseConnection.UploadChanges();
-            }
             
-        }
-
-        private int GetQuantity(object o)
-        {
-            return o != DBNull.Value ? Convert.ToInt32(o) : 0;
         }
     }
 }
